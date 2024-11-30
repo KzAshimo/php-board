32386bcdc006acf2d953178852ef6b475c7f9b71
KzAshimo <good.ashimo@gmail.com>
2024-11-30 23:12:59 +0900

    解釈をメッセージに

--- seconde.php
+++ seconde.php
@@ -1,26 +1,28 @@
 <?php
-// sessionを使用
+// sessionを使用(データの保存、データベースを使用すれば使用必要なし)
 session_start();
-// post内容を保存（配列に保存）
-if($_SERVER["REQUEST_METHOD"] === "POST" ){
-    $wight = $_POST["going"];
 
-    if(!empty("going")){
-        $_SESSION["goings"][] = $wight;
+// フォームから送信されたデータを受け取り、sessionに保存
+if($_SERVER["REQUEST_METHOD"] === "POST" ){  //ポストメソッドが使用されたら
+    // if($__POST)でダメな理由　⇒左記ではPOSTデータが空かどうかの判定になる（使用された判定×）
+    $wight = $_POST["going"]; //$wightにPOSTデータの"going"を保存
+
+    if(!empty($wight)){ //"weight"が空でないなら(emptyで判定)
+        $_SESSION["goings"][] = $wight; //session配列に保存
     }
     
 }
 // ui
-echo "<form action='seconde.php' method='post'>";
+echo "<form action='seconde.php' method='post'>"; //form second.php内のアクション　post(送信)メソッド
 echo "<label>けいじばん</label><br>";
-echo "<textarea name='going' placeholder='入力'></textarea><br>";
-echo "<button type='submit'>出力</button>";
+echo "<textarea name='going' placeholder='入力'></textarea><br>"; //nameを"going"にして送信
+echo "<button type='submit'>出力</button>"; //submit　でデータ送信ボタン
 echo "</form>";
 
-// post内容をくり返し処理で書き込み処理
+// session内に保存された投稿内容を一覧表示
 echo "<h3>内容</h3>";
-if(!empty($_SESSION["goings"])){
-    foreach($_SESSION["goings"] as $post){
+if(!empty($_SESSION["goings"])){ //goingsが空でないなら
+    foreach($_SESSION["goings"] as $post){ //配列"going"を$postの個数繰り返し処理
         echo "<p>{$post}</p>";
     }
 }
