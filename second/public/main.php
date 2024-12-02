477b12211ed3aeb49a317e2d831265bd14fe018f
KzAshimo <good.ashimo@gmail.com>
2024-12-02 12:55:05 +0900

    ２週目　途中

---
+++ second/public/main.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>title</h1>
+    <form action="content.php" method="post">
+        <textarea name="textarea" placeholder="入力"></textarea>
+        <br>
+        <button type="submit" name="addAText">書込</button>
+        <button type="submit" name="addDelete">削除</button>
+    </form>
+</body>
+</html>
